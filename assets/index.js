c1e6c0b08cf723abfc6ceabdd0e43a365affce13
lilys tacos <lilyannek@gmail.com>
2022-06-28 00:39:28 -0700

    trying new html and css, creation of answer buttons and styling

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,33 @@
 // Starts quiz when button is clicked
-var btn = document.getElementById("startButton")
-button.addEventListener("click", function() {
-    
+var startButton = document.querySelectorAll("#startQuiz")
+startButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Button clicked");
 })
 
+var quizContainer = document.querySelectorAll("quiz");
+
+// Array for quiz questions
+var quizQuestions = [ 
+    {
+    question: "What does HTML stand for?",
+    options: ["Hypertext Markup Language", "Having Many Laughs", "Hypertension Marker Linguistics"],
+    answer: "HypertextMarkup Language"
+    },
+    {
+    question: "A set of statements that performs a task or calculates a value is ___",
+    options: ["An object", "A function", "Scope"],
+    answer: "A function"
+    },
+    {
+    question: "",
+    options: ["Brendan Eich", "Tim Berners-Lee", "Bill Gates"],
+    answer: "Brendan Eich"
+    },
+];
+
+// Variable for penalty when a question is answered incorrectly
+var penalty = 5;
 // Creates timer for countdown of quiz
 var timer = document.getElementById("countdown");
 
@@ -12,50 +36,16 @@ function countdown() {
 
     var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
-            timer.textContent = "Time: " + timeLeft;
+            timeLeft.textContent = "Time: " + timeLeft;
             timeLeft--;
-        } else {
-            timer.textContent = "";
+        } else if (timeLeft === 0) {
             clearInterval(timeInterval);
+            timeLeft.textContent = "You're time is up!";
         }
     }, 1000);
 }
 
 countdown();
-
-var quizContainer = document.getElementById("quiz");
-var resultsContainer = document.getElementById("results");
-var submitBtn = document.getElementById("submit");
-
-// Array for quiz questions
-var quizQuestions = [ {
-    question: "What does HTML stand for?",
-    answer: {
-        a: "Hypertext Markup Language",
-        d: "Having Many Laughs",
-        c: "Hypertension Marker Linguistics"
-    },
-    correctAnswer: "a"
-    },
-    {
-    question: "A set of statements that performs a task or calculates a value is ___",
-    answer: {
-        a: "An object",
-        b: "A function",
-        c: "Scope"
-    },
-    correctAnswer: "b"
-    },
-    {
-    question: "",
-    answer: {
-        a: "Brendan Eich",
-        b: "Tim Berners-Lee",
-        c: "Bill Gates"
-    },
-    correctAnswer: "a"
-    },
-];
 
 // Function to display the quiz questions in order
 function quizQuestions(){
@@ -63,3 +53,17 @@ function quizQuestions(){
 }
 
 submitBtn.addEventListener("click", showResults);
+
+// Displaying scores and initials at the end 
+// function getScore(form) {
+//     showCorrectAnswers();
+
+//     var score = 0;
+//     for (i = 0, i++);
+// }
+
+
+var highScore = document.querySelector("#highscore");
+
+var totalScores = localStorage.setItem("totalScores");
+localStorage.setItem("totalScores", totalScores);
