a30aa403531d950823f9f515219abe459bd5d561
lilys tacos <lilyannek@gmail.com>
2022-06-28 18:57:22 -0700

    addition of event listeners to question buttons to queue next question, for loop to display previous scores and initials by looping through local storage, refactoring of JS

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,30 @@
 // Starts quiz when button is clicked
 var startBtn = document.getElementById("startQuiz")
+var questionContainerEl = document.getElementById("questionContainer")
+var questionEl = document.getElementById("questions")
+var answerOptionsEl = document.getElementById("answerOptions")
+var btn1 = document.getElementById("btn1")
+var btn2 = document.getElementById("btn2")
+var btn3 = document.getElementById("btn3")
+var timeLeft = 60;
+var penalty = 5;
+var scoreList = document.getElementById('highscore')
+
+
 startBtn.addEventListener("click", startQuiz) 
-
-function startQuiz() {
-    console.log("Quiz has started");
-    startBtn.classList.add("hide");
-}
-
-function setNextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
+btn1.addEventListener("click", checkAnswer)
+btn1.addEventListener("click", setNextQuestion)
+btn2.addEventListener("click", checkAnswer)
+btn2.addEventListener("click", setNextQuestion)
+btn3.addEventListener("click", checkAnswer)
+btn3.addEventListener("click", setNextQuestion)
 
 // Array for quiz questions
 var quizQuestions = [ 
     {
     question: "What does HTML stand for?",
     options: ["Hypertext Markup Language", "Having Many Laughs", "Hypertension Marker Linguistics"],
-    answer: "HypertextMarkup Language"
+    answer: "Hypertext Markup Language"
     },
     {
     question: "A set of statements that performs a task or calculates a value is ___",
@@ -28,32 +32,69 @@ var quizQuestions = [
     answer: "A function"
     },
     {
-    question: "",
+    question: "Who invented Javascript?",
     options: ["Brendan Eich", "Tim Berners-Lee", "Bill Gates"],
     answer: "Brendan Eich"
     },
 ];
 
-// Variable for penalty when a question is answered incorrectly
-var penalty = 5;
+var i = 0
+
+function setNextQuestion() {
+    if(i===quizQuestions.length){
+        clearInterval(window.timeInterval)
+        var userInit = prompt("enter your initials")
+        localStorage.setItem(userInit, timeLeft)
+        questionContainerEl.classList.add("hide");
+        window.timeMessage.classList.add("hide");
+        for (index=0;index<localStorage.length;index++){
+        var li = document.createElement('li')
+        scoreList.appendChild(li)
+        li.textContent = localStorage.key(index) + ': ' + localStorage.getItem(localStorage.key(index))
+        }
+        return
+    }
+    questionEl.textContent = quizQuestions[i].question
+    btn1.innerText = quizQuestions[i].options[0]
+    btn2.innerText = quizQuestions[i].options[1]
+    btn3.innerText = quizQuestions[i].options[2]
+    i++
+    console.log(i)
+}
+
+function checkAnswer(e) {
+    
+    if (e.target.innerText !== quizQuestions[i-1].answer) {
+        timeLeft -= penalty
+    } 
+    
+}
+
+function startQuiz() {
+    console.log("Quiz has started");
+    startBtn.classList.add("hide");
+    questionContainerEl.classList.remove("hide");
+    document.querySelector('.intro').classList.add("hide");
+    countdown();
+    setNextQuestion();
+} 
+
 // Creates timer for countdown of quiz
 var timer = document.getElementById("countdown");
 
 function countdown() {
-    var timeLeft = 60;
+    window.timeMessage = document.getElementById("countdown")
 
-    var timeInterval = setInterval(function() {
+     window.timeInterval = setInterval(function() {
         if (timeLeft > 1) {
-            timeLeft.textContent = "Time: " + timeLeft;
+            window.timeMessage.textContent = "Time: " + timeLeft;
             timeLeft--;
         } else if (timeLeft === 0) {
-            clearInterval(timeInterval);
-            timeLeft.textContent = "You're time is up!";
+            clearInterval(window.timeInterval);
+            window.timeMessage.textContent = "You're time is up!";
         }
     }, 1000);
 }
-
-countdown();
 
 
 // var highScore = document.querySelector("#highscore");
